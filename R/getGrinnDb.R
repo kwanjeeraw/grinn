666f8cdbfa3309e779389f657991cad9ac39f4df
kwanjeeraw <kwanjeeraw@gmail.com>
2015-09-23 13:08:45 -0700

    edit wording

--- R/getGrinnDb.R
+++ R/getGrinnDb.R
@@ -1,9 +1,9 @@
-#'Get grinn DB location
-#'@description get grinn DB location of the current working envornment. 
-#'By default grinn DB lacation is http://grinn.genomecenter.ucdavis.edu:7474/db/data/cypher 
+#'Get Grinn database location
+#'@description get Grinn database location of the current working envornment. 
+#'By default Grinn database lacation is http://grinn.genomecenter.ucdavis.edu:7474/db/data/cypher 
 #'which contains only human database.
 #'@usage getGrinnDb()
-#'@return string grinn DB location.
+#'@return url of Grinn database location.
 #'@author Kwanjeera W \email{kwanich@@ucdavis.edu}
 #'@export
 getGrinnDb <- function(){
--- R/setGrinnDb.R
+++ R/setGrinnDb.R
@@ -1,10 +1,11 @@
-#'Set grinn DB location
-#'@description set grinn DB location for the currently working environment.
+#'Set Grinn database location
+#'@description set Grinn database location for the currently working environment.
 #'@usage setGrinnDb()
-#'@param url grinn DB location
+#'@param url of Grinn database location
 #'@author Kwanjeera W \email{kwanich@@ucdavis.edu}
 #'@export
 #'@examples
+#'# Set Grinn database location to local server
 #'setGrinnDb("http://localhost:7474/db/data/cypher")
 setGrinnDb <- function(url){
   assign("nld", url, envir = .GlobalEnv)
